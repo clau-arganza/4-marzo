9990b388c6ee8b0b46faefa7520f1d24d615d4ba
Caarloos007 <cmagrase@myuax.com>
2026-03-04 19:45:01 +0100

    .

--- cola/consumer-claudia.py
+++
@@ -1,8 +0,0 @@
-import time
-
-ruta="cola"
-
-while True:
-    #cojo el archivo más antiguo de la cola
-    time.sleep(1)
-    print("Soy Claudia y consumo un archivo de Zara")
--- cola/producer-carlos.py
+++ cola/producer-carlos.py
@@ -1,19 +1,17 @@
 import os
 import time
 
-
 QUEUE_DIR = "cola"
+counter = 0
 
 print("[Carlos] Iniciando...")
 
-counter = 0
-
 while True:
-    filename = f"p2_{counter}.txt"
+    filename = f"p1_{counter}.txt"
     filepath = os.path.join(QUEUE_DIR, filename)
 
     with open(filepath, "w") as f:
-        f.write(f"Generado por Producer 2\n")
+        f.write(f"Generado por Producer 1\n")
 
     print(f"[Carlos] Generó {filename}")
     
