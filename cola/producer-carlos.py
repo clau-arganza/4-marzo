3f911b00efe6ac9e4bb6ae3f37b06e2d2c13374a
Caarloos007 <cmagrase@myuax.com>
2026-03-04 19:50:05 +0100

    ,

--- cola/producer-carlos.py
+++ cola/producer-carlos.py
@@ -11,7 +11,7 @@ while True:
     filepath = os.path.join(QUEUE_DIR, filename)
 
     with open(filepath, "w") as f:
-        f.write(f"Generado por Producer 1\n")
+        f.write(f"Generado por Carlos 1\n")
 
     print(f"[Carlos] Generó {filename}")
     
